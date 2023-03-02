d0614c6b86dfd2fac36182befc33d4fd5a7a6f91
juko22 <97848579+juko22@users.noreply.github.com>
2023-03-02 19:05:23 +0200

    Added page UI

---
+++ src/pages/AddingNote.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {IonPage} from "@ionic/react";
+
+const AddingNote: React.FC = () => {
+    return (
+        <IonPage>
+            ...
+        </IonPage>
+    );
+};
+
+export default AddingNote;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonCard, 
+  IonCardContent, IonCardTitle, IonButton, IonIcon, IonFab, IonFabButton, 
+  IonNavLink, IonNav, IonRouterOutlet, useIonRouter, IonCol, IonRow
+} from '@ionic/react';
+import React from 'react';
+
 import './Home.css';
+import trash from '../drawables/Trash.svg';
+import add from '../drawables/Add.svg';
+
+import AddingNote from './AddingNote';
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
+
+const notes = [
+  {title: 'Pick up dry cleaning', text: 'Go to freedom street 17a and pick the suit from dry cleaning'},
+  {title: 'Do homework', text: 'Solve math problem'},
+  {title: 'Do homework', text: 'Solve math problem'},
+  {title: 'Do homework', text: 'Solve math problem'},
+  {title: 'Do homework', text: 'Solve math problem'}
+];
 
 const Home: React.FC = () => {
+
+  const navigation = useIonRouter();
+
+  var listItems = notes.map((note, index) =>
+    <IonCard color={"primary"} key={index}>
+      <IonCardTitle>{note.title}</IonCardTitle>
+      <IonCardContent>
+        {note.text}
+      </IonCardContent>
+      <IonButton  color={'danger'}>
+        Delete
+        <IonIcon slot="end" icon={trash}></IonIcon>
+      </IonButton>
+    </IonCard>
+  );
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Notes</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen={true} className="ion-padding">
+        <IonList>
+          {listItems}
+        </IonList>
       </IonContent>
+      <IonRow>
+  <IonCol>
+    <div className="ion-float-end">
+      <IonNavLink routerDirection="forward" component={AddingNote}>
+        <IonFabButton onClick={() => {navigation.push('./AddingNote')}}>
+          <IonIcon icon={add}></IonIcon>
+        </IonFabButton>
+      </IonNavLink>
+      </div>
+  </IonCol>
+</IonRow>
+
     </IonPage>
   );
 };
